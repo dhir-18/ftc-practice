0cddad6da0fedc765a627e7201f6053c8b1b1905
dhir-18 <dhirkshah@gmail.com>
2026-09-13 13:55:52 +0100

    Add UseRobotLocation

--- UseRobotLocation.java
+++ UseRobotLocation.java
@@ -17,6 +17,7 @@ public class UseRobotLocation extends OpMode {
     @Override
     public void loop() {
 
+        // A/B nudge the heading left/right by a small fixed amount each loop while held
         if (gamepad1.a) {
             robotLocationPractice.turnRobot(0.1);
         }
@@ -25,9 +26,10 @@ public class UseRobotLocation extends OpMode {
             robotLocationPractice.turnRobot(-0.1);
         }
 
-        telemetry.addData("Heading", robotLocationPractice.getHeading());
-        telemetry.addData("Angle", robotLocationPractice.getAngle());
+        telemetry.addData("Heading", robotLocationPractice.getHeading()); // normalized -180..180
+        telemetry.addData("Angle", robotLocationPractice.getAngle()); // raw, unbounded
 
+        // d-pad left/right nudge the x-position the same way
         if (gamepad1.dpad_left) {
             robotLocationPractice.changeX(-0.1);
         }
