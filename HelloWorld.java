9b6f45f315e9705206bfe0382c13b30bf20ceea1
dhir-18 <dhirkshah@gmail.com>
2026-09-13 13:55:52 +0100

    Add HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,12 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class HelloWorld extends OpMode { // public all places can see this class
 
+    // init() runs once when the driver hits INIT, before START is pressed
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello", "World"); // shows "Hello: World" on the Driver Station
 
     }
 
+    // loop() runs repeatedly (about 50x/second) after START is pressed
     @Override
     public void loop() {
 
