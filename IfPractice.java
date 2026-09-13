0b0c49be9b0aaf148b777e5642ff2a09dc8233d3
dhir-18 <dhirkshah@gmail.com>
2026-09-13 13:55:52 +0100

    Add IfPractice

--- IfPractice.java
+++ IfPractice.java
@@ -13,8 +13,8 @@ public class IfPractice extends OpMode {
 
     @Override
     public void loop() {
-        boolean aButton = gamepad1.a;
-        if (aButton == true) {
+        boolean aButton = gamepad1.a; // true while the A button is held down
+        if (aButton == true) { // could also just write "if (aButton)" since it's already a boolean
             telemetry.addLine("A button pressed");
         }
 
